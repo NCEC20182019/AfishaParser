d72966d0186336d488918f214789aed6c01537de
Vitaliy <safonov.vitvrn@gmail.com>
2019-04-30 17:28:33 +0300

    new path

--- src/main/java/rest_service/PostToEventService.java
+++ src/main/java/rest_service/PostToEventService.java
@@ -10,10 +10,10 @@ import java.util.ArrayList;
 
 public class PostToEventService {
 //Выставить необходимый урл
-    private static final String TEST_EVENT_SERVICE_URL = "http://localhost:8094/parser/event";
-    private static final String REAL_EVENT_SERVICE_URL = "http://localhost:8092/event/create/";
-    private static final String SERVER_EVENT_SERVICE_URL = "lemmeknow.tk:8092/event/create/";
-    private static final String SERVER1_EVENT_SERVICE_URL = "/event/create";
+    private static final String TEST_EVENT_SERVICE_URL    =  "http://localhost:8094/parser/event";
+    private static final String REAL_EVENT_SERVICE_URL    =  "http://localhost:8092/event/create/";
+    private static final String SERVER_EVENT_SERVICE_URL  =  "http://lemmeknow.tk:8092/event/create/";
+    private static final String SERVER1_EVENT_SERVICE_URL =  "/event/create";
 
     private PostToEventService(){};
 
@@ -25,7 +25,7 @@ public class PostToEventService {
         // Data attached to the request.
         HttpEntity<Event> requestBody = new HttpEntity<>(event, headers);
         // Send request with POST method.
-        Event e = rt.postForObject(SERVER1_EVENT_SERVICE_URL, requestBody, Event.class);
+        Event e = rt.postForObject(SERVER_EVENT_SERVICE_URL, requestBody, Event.class);
 //для теста
     }
 
